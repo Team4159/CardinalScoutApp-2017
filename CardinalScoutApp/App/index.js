dff508afac4e40c6141aabc8cc9ed73bd93bd427
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-02-10 18:57:04 -0800

    Previous app's build failed for some reason, so i reinit the app

--- CardinalScoutApp/App/index.js
+++ CardinalScoutApp/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import Home from './scenes/Home'
+import Home from './Scenes/Home'
 class CardinalScoutApp extends React.Component{
   render(){
   return (
