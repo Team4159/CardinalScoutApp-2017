3f722f7670ae12e67732e290ac375e41c156f0d3
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-02-15 09:39:44 -0800

    add redux-remote-devtools as a dev dependency

--- App/config/configureStore.js
+++ App/config/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
-
+import devToolsEnhancer from 'remote-redux-devtools';
 export default function configureStore(){
   /* eslint-disable no-undef */
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, devToolsEnhancer());
   if(module.hot){
     module.hot.accept(() => {
       const nextRootReducer = require('../reducers/index').default;
--- App/index.js
+++ App/index.js
@@ -3,8 +3,7 @@ import { AppRegistry } from 'react-native';
 import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
 import NavRootContainer from './navigation/NavRootContainer';
-import rootReducer from './reducers';
-const store = configureStore(rootReducer)
+const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
