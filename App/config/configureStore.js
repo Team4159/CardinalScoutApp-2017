c67dead36a569a5558bbbb7fe89ac3a2821de2ea
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-02-17 18:14:48 -0800

    add react-native debugger

--- App/config/configureStore.js
+++ App/config/configureStore.js
@@ -1,9 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
-import devToolsEnhancer from 'remote-redux-devtools';
 export default function configureStore(){
   /* eslint-disable no-undef */
-  const store = createStore(rootReducer, devToolsEnhancer());
+  const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   if(module.hot){
     module.hot.accept(() => {
       const nextRootReducer = require('../reducers/index').default;
