2d1e59ec20861c5cf4aa7473a73ec13195fe3761
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-02-20 12:02:20 -0800

    ha

--- App/config/configureStore.js
+++ App/config/configureStore.js
@@ -6,8 +6,10 @@ import { persistStore, autoRehydrate } from 'redux-persist';
 ))*/
 export default function configureStore(){
   /* eslint-disable no-undef */
-  //const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, composeEnhancers(
+    autoRehydrate()
+  ));
   if(module.hot){
     module.hot.accept(() => {
       const nextRootReducer = require('../reducers/index').default;
