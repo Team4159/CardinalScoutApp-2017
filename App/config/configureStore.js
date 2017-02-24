0e3a2ff32f662fcd567513e9cb7a89b0fc7ac675
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-02-24 09:30:28 -0800

    fixed a bug where webView wasn't passing. Tests should pass now.

--- App/config/configureStore.js
+++ App/config/configureStore.js
@@ -5,7 +5,7 @@ import { persistStore, autoRehydrate } from 'redux-persist';
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 export default function configureStore(){
   /* eslint-disable no-undef */
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  //const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(rootReducer, compose(autoRehydrate()));
   if(module.hot){
     module.hot.accept(() => {
--- __tests__/index.js
+++ __tests__/index.js
@@ -4,7 +4,7 @@ import App from '../App/index';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-
+jest.unmock('ScrollView')
 it('renders correctly', () => {
   const tree = renderer.create(
     <App />
